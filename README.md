f0e09cd9cc79c40554f70b32c53cc55b384ae489
amankumar-arya996 <akarya996@gmail.com>
2025-05-06 10:50:48 +0530

    added files of weather web

---
+++ README.md
@@ -0,0 +1,7 @@
+# simple weather wesite 
+# using HTML ,CSS, Javascript and API
+Used weatherbit api to fetch the weather data
+# please use your api key of weatherbit api
+to use you have to simply search the city by name and you will get the data
+
+
---
+++ weather.js
@@ -0,0 +1,38 @@
+    let apiKey= "";//your api key
+    let btn = document.querySelector("button");
+
+    btn.addEventListener("click", async () => {
+        let city = document.querySelector("input").value;
+        let weatherData = await getWheather(city);
+        show(city, weatherData);
+    });
+    
+    async function getWheather(city) {
+        try {
+            let url = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${apiKey}`;
+            let res = await axios.get(url);
+            return res.data.data[0]; // return full weather object
+            // console.log(res.data.data[0]) ; // return full weather object
+
+        } catch (e) {
+            console.log("error", e);
+            return null;
+        }
+    }
+    
+    function show(city, weather) {
+        let result = document.querySelector("#result");
+        if (!weather) {
+            result.innerText = "Weather data not available.";
+            return;
+        }
+    
+        result.innerHTML = `
+            <h3>Weather in ${city} , ${weather.country_code}</h3>
+            <p><strong>Temperature:</strong> ${weather.app_temp}°C</p>
+            <p><strong>Humidity:</strong> ${weather.rh}%</p>
+            <p><strong>Wind Speed:</strong> ${weather.wind_spd} m/s</p>
+            <p><strong>Description:</strong> ${weather.weather.description}</p>
+        `;
+    }
+    
